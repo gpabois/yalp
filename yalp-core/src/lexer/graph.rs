890f3eea4c953bdb7e95c106e8432672587c75f5
G. PABOIS <gael.pabois@gmail.com>
2024-05-29 15:59:59 +0200

    up

--- yalp-core/src/lexer/graph.rs
+++ yalp-core/src/lexer/graph.rs
@@ -1,254 +1,74 @@
 use super::atomic::Atomic;
 
-pub trait IntoGraphFragment {
-    fn into_graph_fragment(self) -> GraphFragment;
+pub trait IntoGraph {
+    fn into_graph(self) -> Graph;
 }
 
-#[derive(Clone)]
-pub struct Leaf {
-    kind: Atomic,
-}
+pub trait Set {
+    type Item;
 
-impl Leaf {
-    pub fn intersect(&self, rhs: &Self) -> Self {
-        Self {
-            kind: self.kind.intersect(&rhs.kind),
-        }
-    }
-}
-
-impl IntoGraphFragment for Leaf {
-    fn into_graph_fragment(self) -> GraphFragment {
-        let mut fragment = GraphFragment::default();
-        let node = fragment.add_node();
-        fragment.entering(node, self);
-        fragment
-    }
-}
-
-pub enum Expr {
-    Sequence(Sequence),
-    Either(Either),
-    Quantified(Quantified),
-    Leaf(Leaf),
-}
-
-impl IntoGraphFragment for Expr {
-    fn into_graph_fragment(self) -> GraphFragment {
-        match self {
-            Expr::Sequence(seq) => seq.into_graph_fragment(),
-            Expr::Either(_) => todo!(),
-            Expr::Quantified(_) => todo!(),
-            Expr::Leaf(_) => todo!(),
-        }
-    }
-}
-
-/// A1..An
-pub struct Sequence(Vec<Expr>);
-
-impl IntoIterator for Sequence {
-    type Item = Expr;
-    type IntoIter = <Vec<Expr> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl IntoGraphFragment for Sequence {
-    fn into_graph_fragment(self) -> GraphFragment {
-        self.into_iter()
-            .map(IntoGraphFragment::into_graph_fragment)
-            .reduce(|a, b| a + b)
-            .unwrap_or_default()
-    }
-}
-
-pub struct Either(Vec<Expr>);
-
-impl IntoIterator for Either {
-    type Item = Expr;
-    type IntoIter = <Vec<Expr> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl IntoGraphFragment for Either {
-    fn into_graph_fragment(self) -> GraphFragment {
-        self.into_iter()
-            .map(IntoGraphFragment::into_graph_fragment)
-            .reduce(GraphFragment::merge)
-            .unwrap_or_default()
-    }
-}
-
-pub struct Group(Box<Expr>, Option<String>);
-
-pub enum Quantifier {
-    /// *, or {0,}
-    Wild,
-    /// ?, or {0,1}
-    Optional,
-    /// {n,}
-    RangeFrom(usize),
-    /// {,m}
-    RangeTo(usize),
-    /// {n,m}
-    Range(usize, usize),
-}
-pub struct Quantified(Box<Expr>, Quantifier);
-
-pub struct EnteringEdge {
-    leaf: Leaf,
-    to: usize,
-}
-
-#[derive(Clone)]
-pub struct RightArrowSet(Vec<RightArrow>);
-
-impl RightArrowSet {
-    pub fn zip(left: Self, right: Self) -> impl Iterator<Item = (RightArrow, RightArrow)> {
-        left.0.into_iter().flat_map(move |left| {
-            right
-                .clone()
-                .0
-                .into_iter()
-                .map(move |right| (left.clone(), right.clone()))
-        })
-    }
-    pub fn merge(left: Self, right: Self) -> MergedRightEdgeSet {
-        Self::zip(left, right).map(|(left, right)| {
-            let left_leaf = left.leaf.difference(&right.leaf);
-            let right_leaf = right.leaf.difference(&left.leaf);
-            let common_leaf = left.leaf.intersect(&right.leaf);
-        });
-    }
-}
-
-pub struct MergedRightEdgeSet {
-    pub left: RightArrowSet,
-    pub right: RightArrowSet,
-    pub common: Vec<MergedRightArrow>,
-}
-
-pub struct MergedRightArrow {
-    pub leaf: Leaf,
-    // (left, right)
-    pub merge: [usize; 2],
-}
-
-#[derive(Clone)]
-pub struct RightArrow {
-    leaf: Leaf,
-    to: usize,
-}
-
-impl From<EnteringEdge> for RightArrow {
-    fn from(value: EnteringEdge) -> Self {
-        Self {
-            leaf: value.leaf,
-            to: value.to,
-        }
-    }
-}
-
-impl EnteringEdge {
-    pub fn intersect(&self, rhs: &Self) -> Leaf {
-        self.leaf.intersect(&rhs.leaf)
-    }
-}
-
-impl std::ops::AddAssign<usize> for EnteringEdge {
-    fn add_assign(&mut self, rhs: usize) {
-        self.to += rhs
-    }
-}
-
-pub struct LeavingEdge {
-    from: usize,
-}
-
-impl LeavingEdge {
-    pub fn connect(&self, rhs: &EnteringEdge) -> Edge {
-        Edge {
-            from: self.from,
-            leaf: rhs.leaf.clone(),
-            to: rhs.to,
-        }
-    }
-}
-
-impl std::ops::AddAssign<usize> for LeavingEdge {
-    fn add_assign(&mut self, rhs: usize) {
-        self.from += rhs
-    }
-}
-
-pub struct Edge {
-    from: usize,
-    leaf: Leaf,
-    to: usize,
-}
-
-impl std::ops::AddAssign<usize> for Edge {
-    fn add_assign(&mut self, rhs: usize) {
-        self.to += rhs;
-        self.from += rhs;
-    }
+    fn intersect(lhs: Self, rhs: Self) -> Self;
+    fn union(lhs: Self, rhs: Self) -> Self;
+    fn difference(lhs: Self, rhs: Self) -> Self;
+    fn is_empty(&self) -> Self;
+    fn contains(&self, item: &Self::Item) -> bool;
 }
 
 #[derive(Default)]
-pub struct GraphFragment {
-    entering: Vec<EnteringEdge>,
-    leaving: Vec<LeavingEdge>,
+pub struct Graph {
     edges: Vec<Edge>,
-
     offset: usize,
     count: usize,
 }
 
-impl GraphFragment {
-    pub fn add_node(&mut self) -> usize {
+impl Graph<A> where A: Clone {
+    pub fn add(&mut self) -> Node {
         let node = self.count;
         self.count += 1;
-        node
+        node.into()
     }
 
-    pub fn connect(&mut self, from: usize, leaf: Leaf, to: usize) {
-        self.edges.push(Edge { from, to, leaf })
+    pub fn iter_entering_edges(&self) -> impl Iterator<Item=&Edge<A>> {
+        self.edges.iter().filter(|edge| edge.from.is_start())
     }
 
-    pub fn entering(&mut self, to: usize, leaf: Leaf) {
-        self.entering.push(EnteringEdge { leaf, to })
+    pub fn iter_leaving_edges(&self) -> impl Iterator<Item=&Edge<A>> {
+        self.edges.iter().filter(|edge| edge.to.is_end())
     }
 
-    pub fn leaving(&mut self, from: usize) {
-        self.leaving.push(LeavingEdge { from })
+    pub fn iter_internal_edges(&self) -> impl Iterator<Item=&Edge<A>> {
+        self.edges.iter().filter(|edge| edge.to.is_internal() && edge.from.is_internal())
     }
 
     pub fn offset(&mut self, n: usize) {
         self.offset += n;
-        self.entering.iter_mut().for_each(|edge| *edge += n);
         self.edges.iter_mut().for_each(|edge| *edge += n);
-        self.leaving.iter_mut().for_each(|edge| *edge += n);
     }
 
+    /// Append two graphes
     pub fn append(&mut self, mut rhs: Self) {
         rhs += self.offset + self.count;
 
         self.count += rhs.count;
-        self.edges.extend(rhs.edges);
+        let mut edges: Vec<Edge<A>> = vec![];
+
+        edges.extend(self.iter_entering_edges().cloned());
+        edges.extend(self.iter_internal_edges().cloned());
+        edges.extend(rhs.iter_internal_edges().cloned());
+        edges.extend(rhs.iter_leaving_edges().cloned());
+
         // connect lhs::leaving to rhs::entering
-        self.edges.extend(self.leaving.iter().flat_map(|leaving| {
-            rhs.entering
-                .iter()
-                .map(|entering| leaving.connect(entering))
+        edges.extend(self.iter_leaving_edges().flat_map(|leaving| {
+            rhs.iter_entering_edges().map(|entering| {
+                Edge {
+                    from: leaving.from,
+                    action: entering.action.clone() + leaving.action.clone(),
+                    to: entering.to
+                }
+            })
         }));
-        // leaving of the summed fragments is the rhs's leaving.
-        self.leaving = rhs.leaving;
+
+        self.edges = edges;
     }
 
     pub fn merge(mut self, mut rhs: Self) -> Self {
@@ -260,18 +80,155 @@ impl GraphFragment {
 }
 
 /// Offset the fragment
-impl std::ops::AddAssign<usize> for GraphFragment {
+impl std::ops::AddAssign<usize> for Graph {
     fn add_assign(&mut self, rhs: usize) {
         self.offset(rhs)
     }
 }
 
 /// Append two fragments
-impl std::ops::Add<GraphFragment> for GraphFragment {
+impl std::ops::Add<Graph> for Graph {
     type Output = Self;
 
-    fn add(mut self, rhs: GraphFragment) -> Self::Output {
+    fn add(mut self, rhs: Graph) -> Self::Output {
         self.append(rhs);
         self
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum Node {
+    Start,
+    Internal(usize),
+    End
+}
+
+impl Node {
+    pub fn is_start(&self) -> bool {
+        matches!(self, Node::Start)
+    }
+
+    pub fn is_internal(&self) -> bool {
+        matches!(self, Node::Internal(_))
+    }
+
+
+    pub fn is_end(&self) -> bool {
+        matches!(self, Node::End)
+    }
+}
+
+impl From<usize> for Node {
+    fn from(value: usize) -> Self {
+        Self::Internal(value)
+    }   
+}
+
+impl std::ops::AddAssign<usize> for Node {
+    fn add_assign(&mut self, rhs: usize) {
+        match self {
+            Node::Internal(value) => *value += rhs,
+            _ => {}
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Edge<C, A> {
+    from: Node,
+    condition: C,
+    action: Action<A>,
+    to: Node,
+}
+
+
+impl<C,A> std::ops::Mul<Self> for Edge<C,A> 
+where C: Set
+{
+    type Output = Vec<CrossEdge<C, A>>;
+
+    fn mul(self, rhs: Self) -> Vec<CrossEdge<C, A>> 
+    {
+        let left = CrossEdge {
+            from: CrossNode::Left(left.from),
+            // E1 - E2
+            condition: left.condition.clone().difference(right.condition.clone()),
+            to: CrossNode::Left(left.to)
+        };
+
+        let right = CrossEdge {
+            from: CrossNode::Right(right.from),
+            // E2 - E1
+            condition: right.condition.clone().difference(left.condition.clone()),
+            to: CrossNode::Right(right.to)
+        };
+
+        let shared = CrossEdge {
+            from: CrossNode::Shared(left.from, right.from),
+            condition: right.condition.clone().intersect(left.condition.clone()),
+            to: CrossNode::Shared(left.to, right.to)
+        };
+        
+        [left, right, shared].into_iter().filter(|e| !e.is_empty()).collect()
+    }
+}
+
+impl std::ops::AddAssign<usize> for Edge {
+    fn add_assign(&mut self, rhs: usize) {
+        self.to += rhs;
+        self.from += rhs;
+    }
+}
+
+#[derive(Default)]
+pub struct Action<A>(Vec<A>);
+
+//////////////////////////////
+/// Cross graph operations ///
+//////////////////////////////
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub enum CrossNode {
+    Left(Node),
+    Right(Node),
+    Shared(Node, Node)
+}
+
+pub struct CrossEdge<C,A> 
+where C: Set
+{
+    from: CrossNode,
+    condition: C,
+    action: Action<A>,
+    to: CrossNode
+}
+
+
+impl<C,A> CrossEdge<C,A> 
+where C: Set
+{
+    pub fn are_similar(&self, rhs: &Self) -> bool {
+        self.from == rhs.from && self.to == rhs.to
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.condition.is_empty()
+    }
+
+    /// Merge two edges with the same source and destination.
+    /// 
+    /// Actions are added
+    /// Conditions are intersected
+    pub fn merge(self: Self, rhs: Self) -> Self {
+        if self.are {
+            panic!("not mergeable");
+        }
+
+        Self {
+            from: self.from,
+            to: self.to,
+            action: self.action + rhs.action,
+            condition: self.condition.intersect(rhs.condition)
+        }
     }
 }
